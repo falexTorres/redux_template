74c9f733e321858d965cc3046a22bf02d9438b0d
Alex Torres <atorres@fastyeti.com>
2016-11-09 13:53:28 -0600

    finished w redux template

--- redux/index.js
+++ redux/index.js
@@ -1,18 +1,12 @@
+// entry point for app
+import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import todoApp from './reducers'
-import App from './components/App'
+import Root from './containers/Root'
 
 // renders root component to view
 // output file => root.js
-
-let store = createStore(todoApp)
-
 render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
+  <Root />,
   document.getElementById('root')
 )
